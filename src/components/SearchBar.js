702fc1c2144bd157d8838e729483ac5f55b3c9d5
abenson91 <aaronbenson91@gmail.com>
2019-10-29 21:35:08 -0400

    App Finished with Dockerfile

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -30,20 +30,20 @@ class SearchBar extends React.Component {
     };
 
     render() {
-        return (<div className="search-bar ui segment">
-            <form onSubmit={this.onFormSubmit} className="ui form">
-                <div className="field">
-                    <label>Video Search</label>
-                    <input
-                        type="text"
-                        value={this.state.search}
-                        onChange={this.onInputChange}
-                        onClick={this.onClick}
-                        onBlur={this.onBlur}
-                    />
-                </div>
-            </form>
-        </div>);
+            return (<div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.search}
+                            onChange={this.onInputChange}
+                            onClick={this.onClick}
+                            onBlur={this.onBlur}
+                        />
+                    </div>
+                </form>
+            </div>);
 
     }
 
